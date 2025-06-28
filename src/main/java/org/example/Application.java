c0449c4eb9b23a499889a9a9acc745a8d459cb5e
Valeria Pagliarini <valepagliar@gmail.com>
2025-06-28 11:21:18 +0100

    first method added

--- src/main/java/org/example/Application.java
+++ src/main/java/org/example/Application.java
@@ -10,14 +10,29 @@ import java.util.List;
 
 public class Application {
 
-    public static void main(String[] args) {
-        List<Collection> games = new ArrayList<>();
 
-        games.add(new Videogames("VG001", "Skyrim", 2015, 29.99, "Xbox", 100, Genre.ADVENTURE));
-        games.add(new Videogames("VG002", "Last of Us", 2024, 59.99, "PS5", 50, Genre.HORROR));
-        games.add(new Boardgames("BG001", "Risiko", 1995, 35.0, 6, 180));
-        games.add(new Boardgames("BG002", "Uno", 1980, 12.5, 5, 30));
+    static List<Collection> games = new ArrayList<>();
+
+    public static void main(String[] args) {
+
+
+        addGame(new Videogames("VG001", "Skyrim", 2015, 29.99, "Xbox", 100, Genre.ADVENTURE));
+        addGame(new Videogames("VG002", "Last of Us", 2024, 59.99, "PS5", 50, Genre.HORROR));
+        addGame(new Boardgames("BG001", "Risiko", 1995, 35.0, 6, 180));
+        addGame(new Boardgames("BG002", "Uno", 1980, 12.5, 5, 30));
+
     }
 
+    // metodo per aggiungere un gioco evitando
+    public static void addGame(Collection game) {
+        for (Collection g : games) {
+            if (g.getId().equals(game.getId())) {
+                System.out.println("Game with ID '" + game.getId() + "' already exists.");
+                return;
+            }
+        }
+        games.add(game);
+        System.out.println("Game added: " + game.getTitle());
+    }
 
 }
